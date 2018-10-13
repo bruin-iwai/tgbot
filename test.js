cdae0de9f81ab32093113e31afcd990cc08e2087
Katsuhito Iwai <bruin.iwai@gmail.com>
2018-10-13 11:04:42 +0900

    手動テスト削除

--- test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const index = require('./index');
-const event = {
-  'chat_id': -25511902,
-  'text': 'テスト通知です。\nたびたびごめん。'
-};
-index.handler(event)
-  .then(result => console.log(JSON.stringify(result)))
-  .catch(err => console.error(JSON.stringify(err)));

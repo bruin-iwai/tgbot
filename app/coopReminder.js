75cec0b553e82427d8182f0ea4d65d87285e6cd8
Katsuhito Iwai <bruin.iwai@gmail.com>
2018-10-13 12:11:57 +0900

    make unit test

--- app/coopReminder.js
+++ app/coopReminder.js
@@ -1,13 +1,13 @@
 const axiosBase = require('axios');
 
-const axios = axiosBase.create({
-  baseURL: '',
-  headers: {
-    'Content-Type': 'application/json; charset=UTF-8',
-  },
-});
-
 module.exports.handler = async (event) => {
+  const axios = axiosBase.create({
+    baseURL: '',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
+
   const res = await axios.post(process.env.BOT_URI, {
     chat_id: event.chat_id,
     text: event.text,

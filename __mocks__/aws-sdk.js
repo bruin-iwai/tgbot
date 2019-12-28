f0bb98086b750ba90525d312e58ff5ff6725bfa5
Katsuhito Iwai <bruin.iwai@gmail.com>
2019-12-28 19:37:48 +0900

    毎日20時起動とし、ChangeCalendarがopenの時だけtelegram通知するように修正

--- __mocks__/aws-sdk.js
+++ __mocks__/aws-sdk.js
@@ -4,12 +4,18 @@ const mockSSMGetParameter = jest.fn(() => ({
   promise: mockAwsPromise,
 }));
 
+const mockSSMGetCalendarState = jest.fn(() => ({
+  promise: mockAwsPromise,
+}));
+
 const SSM = jest.fn(() => ({
   getParameter: mockSSMGetParameter,
+  getCalendarState: mockSSMGetCalendarState,
 }));
 
 module.exports = {
   mockAwsPromise,
   mockSSMGetParameter,
+  mockSSMGetCalendarState,
   SSM,
 };
